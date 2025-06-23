d3b8a7fba141f5ca33191c4f03d1e09de6b7a758
sankalp <sankalp.nagle@gigatorb.com>
2025-06-23 14:36:37 +0530

    remove file

--- src/app.js
+++ src/app.js
@@ -1,1 +1,17 @@
 require("./scheduler/affirmationScheduler");
+
+
+// {
+//     "type": "service_account",
+//     "project_id": "affirmation-dcb89",
+//     "private_key_id": "5ce43c2b8a35575192d61282a07fa5bdebe705b6",
+//     "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCmMmmDkuM1sNS6\nEV9BdBEZ50NSy7y2ymJyjKAR/wHNw+H3dYQgd9dT9YuTO1NHeLP9s1yx0pGGavgC\nobqIk+ojU7W11MZoMa1ynxTetrm6k/FY/mACuqgmx8TgWOi7uWvD5qQfo5PSDN1L\nvPUn3UsCjXqAbIMh91k5u2OX9odlr5OIpQEY1Rzw5NfD97CULBerjWf5UUhq453p\n6Z8GYlk5b8zBSTKG36ogzj4uRP3osmpgjnDh10XJIt/aixggLWGNu0KShhvvdC5R\njwKt7Fzs6INfI0onBfv6o44AlzxmASDdvGvnfIGD0Ws86BhglBM8yqqaFD86iQaj\nAsctH0dJAgMBAAECggEACW1/VMpmqdYKdd+kaAHIh+VO3rSAqQcusdF5Bxb7KDao\nwTE2ZI0dIZMimXXaZzSD/qvyYdf7D1qkD6ooUF/A/sNcjHWx0kP3KIhjq6/xNedM\nipWA0XF7Nm/1+BqzC8Bx9N6cMtCZrbxdv/0hFGOA7M0lGiyNp8Xwls4i0S0Cnemn\nYy5PfPAnVm0Cz+tzrSGuvRJhEybZcJxxImRmi/DIoiBxBoSk5BeYXYe4E9RZAI47\niaEDMgtX5j2ZB2PWZltsqdG8Yv1kUwXCc4VJIjDUmmr2Er0yZOILKK0T9DUi4sTf\nArofxYCm21iCHAXzz7mqQwo5CVIiEd3/tPbixYm88QKBgQDazygRfo7FROH4j+nQ\nX/6zx9N6HhyWsUPj0lO96eJ72voh03ZD0808mAsaOUlB9pqh8o7U2gO3SYoEZZZN\nXixeEtc2X3ImhJL4ritUSglPMn7VX+G8hl2lJAtnl5I/XgF8xpAUHypQ8tn3w0dL\nW6Ug+jiHwltgYdrh3GJ6ihgiRQKBgQDCcfxfmTKbQL+l0w7AvYdyEZomxGsTf4OO\nvpS8Yp9c/xU07ZI+jMCLV5cS6SIOGlBs+Dt2Fac25Ae6FVdeYZDK7cqT/PgMbyqP\nIzB+734Ms3znYOGq3WKgbGUyJIsHu+yyU43z3mtGq66oQSKo9Wgv+rFwetFbnxXU\nsZsbaxnjNQKBgCw86brvvWnAzXb1ka4hWlt6sRTldZ455KHw6iFhYfrsgNnJ6QU5\nkysHCmeY4WjGtOi9I4eBNHMgGRleeI7X1VyHrp89mNSUxSol/HikJ1Gtwdrd8cOt\n3sz+hpfBGz5YgTB67LJEwsdu8gaXophnxPDv5fH8p4DLB/fWGMrxlIOJAoGBAK91\nsCiL01CpUsbN9ygRXqGeJJ4K7ByCGjK64vhDfk6ql3ZVUnxDApQ6i8ayvNpBAIX0\nUaOy0lsD1b0i+N3f6IpVnVGBGkwOHDdhp6dDcVkAthsMH1bRwufEKGTg+NwXxfT8\n1MFU2TTCQy7s59vS5xs0et1m1nh4y3hMdAx/zrJpAoGAR45hPsMCoiHeUHbOHocm\nCuV1ZqJWFO43idLKJ3b8VhnrOsnxjJnz/yWoysQB1Zw7F5aJVsZfbqzhNdedW9Wc\nG3iSB25VxUqK488EC00Tlg2QaCeEjA9k3LL1o80xl53lix4JGUc2zYcUjQThKljo\nSCRGP+kuT0aqJHXJon/yYjk=\n-----END PRIVATE KEY-----\n",
+//     "client_email": "firebase-adminsdk-fbsvc@affirmation-dcb89.iam.gserviceaccount.com",
+//     "client_id": "113342121638640071372",
+//     "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+//     "token_uri": "https://oauth2.googleapis.com/token",
+//     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+//     "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-fbsvc%40affirmation-dcb89.iam.gserviceaccount.com",
+//     "universe_domain": "googleapis.com"
+//   }
+  
