48793d4d9ccd182a60c9df43ef45254ff448fe43
sankalp <sankalp.nagle@gigatorb.com>
2025-06-25 14:41:29 +0530

    auth

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -150,7 +150,7 @@ router.get("/user/:userId", postController.getUserPosts);
 router.get(
   "/admin/all",
   authenticate,
-  authorizeRoles("ADMIN"),
+  // authorizeRoles("ADMIN"),
   postController.getAllPosts
 );
 
